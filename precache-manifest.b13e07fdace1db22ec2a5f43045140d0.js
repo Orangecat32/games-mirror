2686a036e9e63695e4d074cc45c113644d963da3
Orangecat32 <Orangecat32@gmail.com>
2018-11-23 12:38:09 -0600

    Updates

---
+++ precache-manifest.b13e07fdace1db22ec2a5f43045140d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d75e6370295e046dd0b170ee42c3fa44",
+    "url": "/games-mirror/static/media/icons-16.d75e6370.ttf"
+  },
+  {
+    "revision": "f9b38ca086a60ab3e169",
+    "url": "/games-mirror/static/css/main.fad76b3e.chunk.css"
+  },
+  {
+    "revision": "0f7149769d6b4ee77b646ee5b90c005a",
+    "url": "/games-mirror/static/media/icons-20.0f714976.ttf"
+  },
+  {
+    "revision": "76144f61f703b20c7f3b",
+    "url": "/games-mirror/static/js/1.76144f61.chunk.js"
+  },
+  {
+    "revision": "91fe04662885b7ef14b2",
+    "url": "/games-mirror/static/js/runtime~main.91fe0466.js"
+  },
+  {
+    "revision": "9f52e4014adab067c8ef0f48ec1a4f43",
+    "url": "/games-mirror/static/media/icons-16.9f52e401.eot"
+  },
+  {
+    "revision": "f9b38ca086a60ab3e169",
+    "url": "/games-mirror/static/js/main.f9b38ca0.chunk.js"
+  },
+  {
+    "revision": "c6e7874eefb0dacfd50dad73a2de5a17",
+    "url": "/games-mirror/static/media/icons-20.c6e7874e.eot"
+  },
+  {
+    "revision": "b49a90ee8c246157637b5ed142005d12",
+    "url": "/games-mirror/static/media/icons-16.b49a90ee.woff"
+  },
+  {
+    "revision": "9baec0ab0bc3096fb9fe865d6ae29d1d",
+    "url": "/games-mirror/static/media/icons-20.9baec0ab.woff"
+  },
+  {
+    "revision": "76144f61f703b20c7f3b",
+    "url": "/games-mirror/static/css/1.110698a2.chunk.css"
+  },
+  {
+    "revision": "ff32c7a4874337cbcb1a42b9244c8caf",
+    "url": "/games-mirror/index.html"
+  }
+];
