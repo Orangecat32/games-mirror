6f3868696bbdbfdc03d6eca326add2ba4f027e2c
Orangecat32 <Orangecat32@gmail.com>
2018-11-26 16:57:02 -0600

    memory score part1

--- src/components/MemoryGame/utils.js
+++ src/components/MemoryGame/utils.js
@@ -5,8 +5,8 @@ import {randomArray} from '../../shared/utils';
 export const createCards = (randomArray) => {
   let cards = [];
   for(let i = 0, n = 0; i < ICON_COUNT; i++){
-    cards.push({index:n++, name:iconNames[i]});
-    cards.push({index:n++, name:iconNames[i]});
+    cards.push({index:n++, name:iconNames[i], exposedClicks: []});
+    cards.push({index:n++, name:iconNames[i], exposedClicks: []});
   }
 
   // shuffle the cards based upon the input random array (comes from the function below )
@@ -21,27 +21,77 @@ export const createCards = (randomArray) => {
 export const randomCardArray = () => randomArray(ICON_COUNT * 2);
 
 
-export const clickCard = (cards, card) => {
-  // ignore click if card already turned over or there are two flipped cards already
-  if (card.isFlipped || card.isMatched || hasUnmatched(cards)) {
+export const clickCard = (cards, thisCard, clickCount) => {
+
+  if (thisCard.isFlipped || thisCard.isMatched || hasUnmatched(cards)) {
+    // ignore click if card already turned over or there are two flipped cards already
     return null;
   }
 
-  // is there another flipped card?
-  const flippedCard = cards.find(c => c.isFlipped);
-  if(flippedCard === undefined){
-    //  user clicked on the first card when looking for match, turn card face up
-    return cards.map(c => c.index === card.index ? Object.assign({}, c , {isFlipped: true, isMatched: false}) : c);
-  }
+  // note: prop exposedClicks records how many times this card was exposed before it was matched
 
-  if(flippedCard.name === card.name) {
+  const flippedCard = cards.find(c => c.isFlipped); // is there another flipped card?
+    if(flippedCard === undefined){
+      // there is not another face up unmatched card
+      // user clicked on the first card when looking for match, turn card face up
+      const cc = Object.assign({}, thisCard , {
+        exposedClicks : thisCard.exposedClicks.concat(clickCount),
+        isFlipped: true, 
+        isMatched: false});
+
+      const newCards = cards.map(c => c.index === cc.index ? cc : c);
+      return {cards: newCards, clickedCard: cc };
+    }
+
+  if(flippedCard.name === thisCard.name) {
     // user matched the card, set flipped and matched for those 2 matching cards
-    return cards.map(c => c.name === card.name ? Object.assign({}, c , {isFlipped: false, isMatched: true}) : c);
-  } 
+    const cc = Object.assign({}, thisCard , {isFlipped: false, isMatched: true }) ;
+    const newCards = cards.map(c => c.name !== thisCard.name 
+        ? c 
+        : Object.assign({}, c.index === thisCard.index ? cc : c , {isFlipped: false, isMatched: true}));
+    return {cards: newCards, clickedCard: cc };
+  }
   
   // turn over the second card (no match)
-  return cards.map(c => c.index === card.index ? Object.assign({}, c , {isFlipped: true, isMatched: false}) : c);
+  const cc = Object.assign({}, thisCard , {
+    exposedClicks : thisCard.exposedClicks.concat(clickCount),
+    isFlipped: true, 
+    isMatched: false
+  });
+
+  return {cards: cards.map(c => c.index === cc.index ? cc : c), clickedCard: cc };
 }
 
 export const hasUnmatched = (cards) => cards.filter((c) => c.isFlipped && !c.isMatched).length === 2;
+
+//  unfinished.  usage:  gameScore(state.memoryGame.history)
+export const gameScore = (clickHistory) => {
+  // TODO: compute how well this user performed based upon the click history
+  // work in progress
+
+  let matches = [];
+  for(let i = 0; i < clickHistory.length; i++) {
+    if(clickHistory[i].isMatched){
+      matches.push({
+          name: clickHistory[i].name,
+          first: clickHistory[i-1].exposedClicks.length, 
+          second: clickHistory[i].exposedClicks.length,
+          matchCount: matches.length
+        });
+    }
+  }
+
+  console.log('matches:', matches);
+
+  const result = {
+    luck: matches.filter(m => m.second === 0 && m.matchCount < 11).map(p => p.name), 
+    last: matches.filter(m => m.matchCount === 11).map(p => p.name),  
+    perfect: matches.filter(m => m.second === 1 && m.matchCount < 11).map(p => p.name),  
+    level1: matches.filter(m => m.second === 2 && m.matchCount < 11).map(p => p.name),  
+    level2: matches.filter(m => m.second > 2 && m.matchCount < 11).map(p => p.name),  
+  }
+
+  console.log('result:', result);
+  return 0;
+}
 
--- src/reducers/memoryReducer.js
+++ src/reducers/memoryReducer.js
@@ -5,21 +5,22 @@ import {createCards, clickCard, hasUnmatched} from '../components/MemoryGame/uti
   export function memoryReducer(state, action) {
     switch(action.type) {
       case START_MEMORY_GAME:
-        return Object.assign({}, state, {cards: createCards(action.payload), clickCount: 0, pause: false}); 
+        return Object.assign({}, state, {cards: createCards(action.payload), clickCount: 0, pause: false, history: [] }); 
       case UNFLIP_ALL:
         const ufc = state.cards.map(c => c.isFlipped ? Object.assign({}, c , {isFlipped: false}) : c);
         return  Object.assign({}, state, {cards: ufc, pause: false });
       case CLICK_CARD:
         const clickedCard = action.payload;
-        const newCards = clickCard(state.cards, clickedCard);
-        const hasChanged = (newCards || []).length !== 0;
-        const unmatched = hasUnmatched(newCards);
+        const clickCount = state.clickCount + 1;
+        const result = clickCard(state.cards, clickedCard, clickCount);
+        const hasChanged = (result.cards || []).length !== 0;
+        const unmatched = hasUnmatched(result.cards);
         return !hasChanged ? state : Object.assign({}, state, 
           {
-            cards: newCards, 
-            clickCount: state.clickCount + 1, 
+            cards: result.cards, 
+            clickCount,
             pause: unmatched, 
-            history: state.history.concat(clickedCard)
+            history: state.history.concat(result.clickedCard)
           });
       case SHOW_ALL:
         return Object.assign({}, state, {showAll: !state.showAll});
